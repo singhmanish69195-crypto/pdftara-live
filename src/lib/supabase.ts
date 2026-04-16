c4f75839c9ac343a16d79aff7acd41458d7983f0
manishsingh123166 <mnishs350@gmail.com>
2026-04-16 21:20:02 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gqyqmhgannypuracasdu.supabase.co';
+const supabaseUrl = 'https://qsyesrmjddqcsledxtva.supabase.co';
 // Yahan wo lambi wali key (eyJ...) paste karo
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxeXFtaGdhbm55cHVyYWNhc2R1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNzkwMDYsImV4cCI6MjA4NDc1NTAwNn0.8dlJNPu6jjQt4vcQiaWfypFuB8fSBpv0F3yI1VkMQE4'; 
-
+const supabaseAnonKey = 'sb_publishable_uhzhKdRqKdB5gcXdnxgFsg_tsP875JG';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
